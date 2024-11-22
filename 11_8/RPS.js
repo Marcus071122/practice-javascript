8f3ea37df6a46abb34530075af1123896463378b
Marcus071122 <marcus071105@gmail.com>
2024-11-22 06:14:55 +0000

    11月22日

--- 11_8/RPS.js
+++ 11_8/RPS.js
@@ -1,36 +1,70 @@
 function RPS(){
-    const choices = ["gu","choki","pa","muteki"];
-    //これは配列
-    // let select = document.createElement("select");
-    // let option1 = document.createElement("option");
-    // let option2 = document.createElement("option");
-    // let option3 = document.createElement("option");
-    // const gu = "gu"
-    // option1.value = gu;
-    // option1.textContent = gu;
-    // select.appendChild(option1);
-    // option2.value = "choki";
-    // option2.textContent = "choki";
-    // select.appendChild(option2);
-    // option3.value = "pa";
-    // option3.textContent = "pa";
-    // select.appendChild(option3);
-    // gameContainer.appendChild(select);
+    const choices = ["gu","choki","pa"];
+    let handImage = ""
     let select = document.createElement("select");
+    select.id = "choices"
     choices.forEach(choice => {
         let option = document.createElement("option");
         option.value = choice;
         option.textContent = choice;
+        option.id = choice;
         select.appendChild(option);
     });
     gameContainer.appendChild(select);
 
+    let choiceButton = document.createElement("button")
+    choiceButton.textContent = "決定"
+
+    gameContainer.appendChild(choiceButton);
+
     let image = document.createElement("img");
-    image.alt = "Choice";
-    image.style.width = "500px";
-    image.style.height = "400px";
-    image.src = "https://www.tvlife.jp/wps/wp-content/uploads/2020/05/283937_ec.jpg";
+        image.alt = "Choice";
+        image.style.width = "500px";
+        image.style.height = "400px";
+
+        let enemyImage = document.createElement("img");
+        enemyImage.alt = "Choice";
+        enemyImage.style.width = "500px";
+        enemyImage.style.height = "400px";
+
+    choiceButton.addEventListener("click",function(){
+        
+
+        switch(select.value){
+            case "gu":
+                image.src = "https://i.ytimg.com/vi/aGx6-0dXyXk/maxresdefault.jpg";
+                break;
+            case "choki":
+                image.src = "https://stat.ameba.jp/user_images/20181212/23/bluelittlemoon-vagetable/d2/ea/j/o1080081014319857174.jpg";
+                break;
+            case "pa":
+                image.src = "https://img.cdn.nimg.jp/s/nicovideo/thumbnails/40881712/40881712.78210005.original/r1280x720l?key=f2abb83e56ae154d97e1af9bd68f81437d34669c6429053c42bc357019de7c8d";
+                break;
+            default:
+                break;
+        }
+        let enemyHand = choices[Math.floor(Math.random() * choices.length)];
+        
+
+        switch(enemyHand){
+            case "gu":
+                enemyImage.src = "https://i.ytimg.com/vi/aGx6-0dXyXk/maxresdefault.jpg";
+                break;
+            case "choki":
+                enemyImage.src = "https://stat.ameba.jp/user_images/20181212/23/bluelittlemoon-vagetable/d2/ea/j/o1080081014319857174.jpg";
+                break;
+            case "pa":
+                enemyImage.src = "https://img.cdn.nimg.jp/s/nicovideo/thumbnails/40881712/40881712.78210005.original/r1280x720l?key=f2abb83e56ae154d97e1af9bd68f81437d34669c6429053c42bc357019de7c8d";
+                break;
+            default:
+                break;
+        }
+    })
+
+
+    
     gameContainer.appendChild(image);
+    gameContainer.appendChild(enemyImage);
     //forEach 配列などの複数のデータがあるものに使える。全部やる関数。
     //配列.forEach(配列の中身=>{命令})
 }
